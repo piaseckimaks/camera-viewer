89dce5f5b4f85619fb2a75155368050c9cd934e7
Maksym Piasecki <maksym.piasecki@vionfood.com>
2021-10-22 14:44:11 +0200

    Base of project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            <button>Click me to get my react-create-app from sctratch!!!!!!</button>
+        </div>
+    )
+}
